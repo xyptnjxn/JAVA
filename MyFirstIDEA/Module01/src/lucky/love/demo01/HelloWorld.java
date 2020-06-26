8631dfec4ec6d403d606e8e11a3f37807e1ac6f7
姜运峰 <3170105375@zju.edu.cn>
2020-06-26 11:52:42 +0800

    模拟斗地主

---
+++ MyFirstIDEA/Module01/src/lucky/love/demo01/HelloWorld.java
@@ -0,0 +1,7 @@
+package lucky.love.demo01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ MyFirstIDEA/Module01/src/lucky/love/demo02/Landlord.java
@@ -0,0 +1,50 @@
+package lucky.love.demo02;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Landlord {
+    public static void main(String[] args) {
+        //1.制作扑克
+        ArrayList<String> pokers = new ArrayList<>();
+        String[] types = {"♥", "♠", "♣", "♦"};
+        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        ArrayList<String> playerOne = new ArrayList<>();
+        ArrayList<String> playerTwo = new ArrayList<>();
+        ArrayList<String> playerThree = new ArrayList<>();
+        ArrayList<String> rest = new ArrayList<>();
+        pokers.add("大王");
+        pokers.add("小王");
+        for (String type: types) {
+            for (String number: numbers) {
+                pokers.add(type+number);
+            }
+        }
+        // System.out.println(pokers);
+
+        //2.洗牌
+        Collections.shuffle(pokers);
+        // System.out.println(pokers);
+
+        //3.发牌
+        for (int i = 0; i < pokers.size(); i++) {
+            if (i >= 51) {
+                rest.add(pokers.get(i));
+            } else if (i % 3 == 0) {
+                playerOne.add(pokers.get(i));
+            } else if (i % 3 == 1) {
+                playerTwo.add(pokers.get(i));
+            } else if (i % 3 == 2) {
+                playerThree.add(pokers.get(i));
+            }
+        }
+
+        //4.看牌
+        System.out.println(playerOne);
+        System.out.println(playerTwo);
+        System.out.println(playerThree);
+        System.out.println(rest);
+    }
+}
